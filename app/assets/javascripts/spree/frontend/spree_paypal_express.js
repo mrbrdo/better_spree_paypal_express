ca64de85b492025881352a2cc35a46daaf1bf3de
mrbrdo <mrbrdo@gmail.com>
2022-03-25 01:17:48 +0100

    Instead of having the checkout button separately, replace the Save and Continue cart button with the checkout button when Paypal is selected. Also show a loading indicator while the Paypal redirect is loading.

--- app/assets/javascripts/spree/frontend/spree_paypal_express.js
+++ app/assets/javascripts/spree/frontend/spree_paypal_express.js
@@ -1,6 +1,16 @@
 //= require spree/frontend
 
 SpreePaypalExpress = {
+  $button: null,
+  $buttonParent: null,
+  $newButtonParent: null,
+  initialize: function($button) {
+    this.$button = $button;
+    this.$buttonParent = $button.parent();
+    this.paymentMethodID = $button.data('payment-method-id');
+    this.showLoadingOnClick();
+    this.updateSaveAndContinueVisibility();
+  },
   updateSaveAndContinueVisibility: function() {
     if (this.isButtonHidden()) {
       $(this).trigger('hideSaveAndContinue')
@@ -18,19 +28,40 @@ SpreePaypalExpress = {
   hideSaveAndContinue: function() {
     $("#checkout_form_payment [data-hook=buttons]").hide();
     $("#checkout_form_payment").data('hidden-by-payment-method-id', SpreePaypalExpress.paymentMethodID);
+    this.$newButtonParent = $("#checkout_form_payment [data-hook=buttons]").parent();
+    this.$button.detach().appendTo(this.$newButtonParent);
+    this.$buttonParent.parents('.payment-sources').hide();
   },
   showSaveAndContinue: function() {
     if (typeof ($("#checkout_form_payment").data('hidden-by-payment-method-id')) === 'undefined' || $("#checkout_form_payment").data('hidden-by-payment-method-id') == SpreePaypalExpress.paymentMethodID) {
       $("#checkout_form_payment [data-hook=buttons]").show();
       $("#checkout_form_payment").removeData('hidden-by-payment-method-id');
     }
+    if (this.$newButtonParent) {
+      this.$newButtonParent = null;
+      this.$button.detach().appendTo(this.$buttonParent);
+    }
+  },
+  showLoadingOnClick: function() {
+    var $button = this.$button;
+    var $buttonLink = $button.find('a');
+    $buttonLink.click(function() {
+      $buttonLink.hide();
+      var $loading = $('<span>◯</span>');
+      $loading.appendTo($button);
+      setInterval(function() {
+        if (($loading.text().match(/◯/g) || []).length >= 3)
+          $loading.text('◯');
+        else
+          $loading.text($loading.text() + ' ◯');
+      }, 500);
+    });
   }
 }
 
 Spree.ready(function() {
-  SpreePaypalExpress.paymentMethodID = $('#paypal_button').data('payment-method-id');
-  SpreePaypalExpress.updateSaveAndContinueVisibility();
-  paymentMethods = $('div[data-hook="checkout_payment_step"] input[type="radio"]').click(function (e) {
+  SpreePaypalExpress.initialize($('#paypal_button'));
+  $('div[data-hook="checkout_payment_step"] input[type="radio"]').click(function (e) {
     SpreePaypalExpress.updateSaveAndContinueVisibility();
   });
 })
