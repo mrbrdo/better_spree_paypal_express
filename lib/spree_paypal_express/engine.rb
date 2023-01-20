31d32f4d5463004dce6aae0586e3a3de0e7fce3c
mrbrdo <mrbrdo@gmail.com>
2023-01-20 14:01:13 +0100

    update initializer to work with Spree 4.5+

--- lib/spree_paypal_express/engine.rb
+++ lib/spree_paypal_express/engine.rb
@@ -19,8 +19,8 @@ module SpreePaypalExpress
 
     config.to_prepare &method(:activate).to_proc
 
-    initializer "spree.paypal_express.payment_methods", :after => "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << Spree::Gateway::PayPalExpress
+    config.after_initialize do
+      Rails.application.config.spree.payment_methods << Spree::Gateway::PayPalExpress
     end
   end
 end
