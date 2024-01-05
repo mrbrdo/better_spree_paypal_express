05a33d9ce02ee6a04331cfa70f8d90d69900f407
mrbrdo <mrbrdo@gmail.com>
2024-01-05 22:12:38 +0100

    Add serializer for Spree 4.7 admin

---
+++ app/models/spree/api/v2/platform/paypal_express_checkout_serializer.rb
@@ -0,0 +1,11 @@
+module Spree
+  module Api
+    module V2
+      module Platform
+        class PaypalExpressCheckoutSerializer < BaseSerializer
+          include ResourceSerializerConcern
+        end
+      end
+    end
+  end
+end
--- app/models/spree/paypal_express_checkout.rb
+++ app/models/spree/paypal_express_checkout.rb
@@ -1,4 +1,11 @@
 module Spree
   class PaypalExpressCheckout < ActiveRecord::Base
+    def self.json_api_columns
+      column_names.reject { |c| c.in?(["id"]) }
+    end
+
+    def self.json_api_type
+      to_s.demodulize.underscore
+    end
   end
 end
